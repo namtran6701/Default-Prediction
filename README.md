580674ab5cbe9db552f9953658adb3be758065a1
Nam Tran <nam3864779@gmail.com>
2024-02-29 19:25:29 -0500

    categorical features analysis

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 if missing vals is less than 1 percent, drop 
 5->10% impute with median, mode
 more than that, consider dropping the cols
+
+Categorical notes: 
+term: two terms only - 36 and 60 months -> Convert to 36:0, 60:1
+
+grade: 7 grades - A to G -> Convert to A:0, B:1, C:2, D:3, E:4, F:5, G:6
+
+sub_grade: 35 subgrades - A1 to G5 -> Convert to A1:0, A2:1, A3:2, A4:3, A5:4, B1:5, B2:6, B3:7, B4:8, B5:9, C1:10, C2:11, C3:12, C4:13, C5:14, D1:15, D2:16, D3:17, D4:18, D5:19, E1:20, E2:21, E3:22, E4:23, E5:24, F1:25, F2:26, F3:27, F4:28, F5:29, G1:30, G2:31, G3:32, G4:33, G5:34
+
+home_ownership: 6 types - MORTGAGE, RENT, OWN, OTHER, NONE, ANY -> Convert to MORTGAGE:0, RENT:1, OWN:2, OTHER:3, NONE:4, ANY:5
+
+verification_status: 3 types - Not Verified, Source Verified, Verified -> Convert to Not Verified:0, Source Verified:1, Verified:2
+
+purpose: 14 types - credit_card, debt_consolidation, car, home_improvement, major_purchase, small_business, other, medical, moving, vacation, house, renewable_energy, wedding, educational -> Convert to credit_card:0, debt_consolidation:1, car:2, home_improvement:3, major_purchase:4, small_business:5, other:6, medical:7, moving:8, vacation:9, house:10, renewable_energy:11, wedding:12, educational:13
+
+addr_state: 51 states -> Convert to 51 columns with 1s and 0s
+
+application_type: 2 types - INDIVIDUAL, JOINT -> Convert to INDIVIDUAL:0, JOINT:1
+
+pymnt_plan: 2 types - n, y -> Convert to n:0, y:1
+
+initial_list_status: 2 types - f, w -> Convert to f:0, w:1
+
+
+Numerical notes: 
+
+emp_length: 11 types - less than 1 year, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10+ -> Convert to 0:less than 1 year, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10+:10
+
+int_rate - convert to float
+
+revol_util - convert to float
+
+earliest_cr_line - convert to number of months from earliest credit line to the date of loan application
+
+last_pymnt_d - convert to number of months from last payment date to the date of loan application
+
+last_credit_pull_d - convert to number of months from last credit pull date to the date of loan application
+
+issue_d - convert to number of months from issue date to the date of loan application
+
+
+
+
--- utils/you_never_know_where_it_is.py
+++ utils/you_never_know_where_it_is.py
@@ -27,3 +27,85 @@ def analyze_numerical(df, features):
             
         plt.tight_layout() 
         plt.show()
+        
+        
+# a helper function to examine low cardinality categorical features
+def visualize_categorical(X_train, low_cardinality, figsize=(20, 10)):
+    # Calculate the number of rows needed for subplots
+    n_rows = len(low_cardinality) // 2
+    if len(low_cardinality) % 2: n_rows += 1
+
+    # visualize the histogram of the categorical variables
+    fig, axes = plt.subplots(n_rows, 2, figsize=figsize)
+    axes = axes.flatten()
+
+    for i, col in enumerate(low_cardinality):
+        # print the normalize value counts
+        print(X_train[col].value_counts(normalize=True).mul(100).round(2).astype(str) + '%')
+        print("\n")
+        
+        # Order bars based on their count
+        order = X_train[col].value_counts().index
+        
+        # Create horizontal bars and order them based on their count
+        sns.countplot(data=X_train, y=col, ax=axes[i], color='skyblue', order=order)
+        
+        axes[i].set_title(f"Distribution of {col}")
+
+    # Remove the extra subplot if the number of categories is odd
+    if len(low_cardinality) % 2:
+        fig.delaxes(axes[-1])
+
+    plt.tight_layout()
+    plt.show()
+    
+    # visualize the distribution for large 
+def plot_top_10_frequencies(X_train, large_cat_var, figsize=(20, 20)):
+    top_10_values = {}
+    for feature in large_cat_var:
+        top_10_values[feature] = X_train[feature].value_counts().head(10)
+
+    # Calculate the number of rows needed for subplots
+    n_rows = len(large_cat_var) // 2
+    if len(large_cat_var) % 2: n_rows += 1
+
+    # Create a subplot grid for the histogram plots
+    fig, axes = plt.subplots(n_rows, 2, figsize=figsize)
+
+    # Flatten the axes array if there's more than one row
+    if n_rows > 1:
+        axes = axes.flatten()
+
+    # Plot the frequency of the top 10 values for each categorical feature on the corresponding subplot
+    for i, feature in enumerate(top_10_values.keys()):
+        ax = axes[i]  # Select the appropriate subplot
+        top_10_values[feature].plot(kind='bar', ax=ax, color='skyblue')
+        ax.set_title(f"Top 10 Frequencies of {feature}")
+        ax.set_xlabel("Value")
+        ax.set_ylabel("Frequency")
+        ax.tick_params(axis='x', labelrotation=45)  # Rotate x-axis labels for better readability
+
+    # Remove the extra subplot if the number of features is odd
+    if len(large_cat_var) % 2:
+        fig.delaxes(axes[-1])
+
+    # Adjust layout and spacing
+    plt.tight_layout()
+ 
+
+def plot_boxplot_and_histogram(df, numerical_features, loan_status):
+    # Calculate the number of rows needed for subplots
+    n_rows = len(numerical_features)
+    fig, axes = plt.subplots(n_rows, 2, figsize=(30, 60))  # Change figsize to a larger size
+
+    for i, col in enumerate(numerical_features):
+        # Boxplot on the left
+        sns.boxplot(data=df, x=loan_status, y=col, ax=axes[i, 0])
+        axes[i, 0].set_title(f"Boxplot of {col} vs {loan_status}")
+
+        # Histogram on the right
+        sns.histplot(data=df, x=col, hue=loan_status, ax=axes[i, 1], kde=True)
+        axes[i, 1].set_title(f"Distribution of {col}")
+
+    plt.tight_layout()
+    plt.show()
