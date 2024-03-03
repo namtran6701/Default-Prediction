3d9a67396ecc60d02dfd5a8652b60503962782b2
Nam Tran <nam3864779@gmail.com>
2024-03-02 20:31:18 -0500

    basic model finished

--- README.md
+++ README.md
@@ -9,20 +9,15 @@ grade: 7 grades - A to G -> Convert to A:0, B:1, C:2, D:3, E:4, F:5, G:6
 
 sub_grade: 35 subgrades - A1 to G5 -> Convert to A1:0, A2:1, A3:2, A4:3, A5:4, B1:5, B2:6, B3:7, B4:8, B5:9, C1:10, C2:11, C3:12, C4:13, C5:14, D1:15, D2:16, D3:17, D4:18, D5:19, E1:20, E2:21, E3:22, E4:23, E5:24, F1:25, F2:26, F3:27, F4:28, F5:29, G1:30, G2:31, G3:32, G4:33, G5:34
 
-home_ownership: 6 types - MORTGAGE, RENT, OWN, OTHER, NONE, ANY -> Convert to MORTGAGE:0, RENT:1, OWN:2, OTHER:3, NONE:4, ANY:5
+d: 6 types - MORTGAGE, RENT, OWN, OTHER, NONE, ANY -> Convert to MORTGAGE:0, RENT:1, OWN:2, OTHER:3, NONE:4, ANY:5
 
 verification_status: 3 types - Not Verified, Source Verified, Verified -> Convert to Not Verified:0, Source Verified:1, Verified:2
 
-purpose: 14 types - credit_card, debt_consolidation, car, home_improvement, major_purchase, small_business, other, medical, moving, vacation, house, renewable_energy, wedding, educational -> Convert to credit_card:0, debt_consolidation:1, car:2, home_improvement:3, major_purchase:4, small_business:5, other:6, medical:7, moving:8, vacation:9, house:10, renewable_energy:11, wedding:12, educational:13
-
 addr_state: 51 states -> Convert to 51 columns with 1s and 0s
-
-application_type: 2 types - INDIVIDUAL, JOINT -> Convert to INDIVIDUAL:0, JOINT:1
 
 pymnt_plan: 2 types - n, y -> Convert to n:0, y:1
 
 initial_list_status: 2 types - f, w -> Convert to f:0, w:1
-
 
 Numerical notes: 
 
@@ -40,6 +35,6 @@ last_credit_pull_d - convert to number of months from last credit pull date to t
 
 issue_d - convert to number of months from issue date to the date of loan application
 
-
+desc - convert desc to numerical: 2 types - NaN, not NaN -> Convert to NaN:0, not NaN:1
 
 
--- utils/you_never_know_where_it_is.py
+++ utils/you_never_know_where_it_is.py
@@ -1,6 +1,12 @@
 import warnings
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+
 
 # a helper function to examine the numerical features
 def analyze_numerical(df, features):
@@ -59,7 +65,7 @@ def visualize_categorical(X_train, low_cardinality, figsize=(20, 10)):
     plt.tight_layout()
     plt.show()
     
-    # visualize the distribution for large 
+# visualize the distribution for large 
 def plot_top_10_frequencies(X_train, large_cat_var, figsize=(20, 20)):
     top_10_values = {}
     for feature in large_cat_var:
@@ -109,3 +115,216 @@ def plot_boxplot_and_histogram(df, numerical_features, loan_status):
 
     plt.tight_layout()
     plt.show()
+    
+
+# a helper function to plot the percentage crosstab for categorical features
+def plot_percentage_crosstab(X_train, y_train, low_cardinality):
+    num_vars = len(low_cardinality)
+    num_cols = num_vars // 2 + num_vars % 2 
+
+    # two rows
+    fig, axes = plt.subplots(nrows=2, ncols=num_cols, figsize=(8 * num_cols, 10))
+    axes = axes.flatten()
+
+    # plot heatmap
+    for i, var in enumerate(low_cardinality):
+        crosstab = pd.crosstab(X_train[var], y_train, normalize='columns') * 100
+
+        order = X_train[var].value_counts().index
+
+        sns.heatmap(crosstab.loc[order], annot=True, fmt=".2f", cmap="YlGnBu", ax=axes[i])
+        axes[i].set_title(f'Percentage Crosstab of {var} vs Target')
+        axes[i].set_ylabel(var)
+        axes[i].set_xlabel('Target')
+
+    # hide unused subplots
+    for j in range(num_vars, len(axes)):
+        axes[j].axis('off')
+
+    plt.tight_layout()
+    plt.show()
+    
+# a helper class for feature engineering 
+class DataPreprocessor:
+    def __init__(self):
+        pass
+    # drop some useless numerical cols 
+    def drop_useless_numerical_cols(self, df):
+        
+        useless_cols = ['chargeoff_within_12_mths', 
+                        'collections_12_mths_ex_med', 
+                        'delinq_amnt', 
+                        'tax_liens', 
+                        'policy_code', 
+                        'acc_now_delinq', 
+                        'total_rec_late_fee',
+                        'application_type',
+                        'url', 
+                        'pymnt_plan',
+                        'id',
+                        'member_id',
+                        'zip_code', 
+                        'title',
+                        'pub_rec',
+                        'mths_since_last_record',
+                        'last_pymnt_d',
+                        'loan_amnt'] # highly correlated with funded amnt]
+        
+        # Only keep columns that exist in the DataFrame
+        cols_to_drop = [col for col in useless_cols if col in df.columns]    
+        df = df.drop(cols_to_drop, axis=1)
+        return df
+    # --------------------------------
+    # numerical feature engineering
+    ## 1. convert interest rate cols to numerical from categorical
+    def convert_interest_rate_to_numerical(self, df):
+        df['int_rate'] = df['int_rate'].str.replace('%', '').astype(float)
+        df['revol_util'] = df['revol_util'].str.replace('%', '').astype(float)
+        return df
+    ## 2. emp_length
+    def emp_length_fe(self, df):
+        # convert emp_length to numerical: 11 types - 10+ years, 1 year, 2 years, 3 years, 4 years, 5 years, 6 years, 7 years, 8 years, 9 years, < 1 year -> Convert to 10:10, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 0:0
+        emp_length_dict = {'10+ years': 10, '1 year': 1, '2 years': 2, '3 years': 3, '4 years': 4, '5 years': 5, '6 years': 6, '7 years': 7, '8 years': 8, '9 years': 9, '< 1 year': 0}
+        df['emp_length'] = df['emp_length'].map(emp_length_dict)
+        return df
+    ## 3.mths_since_last_delinq
+    def mths_since_last_delinq_fe(self, df):
+        # assume that if the record is missing, it means the person has never been delinquent
+        df['mths_since_last_delinq'] = df['mths_since_last_delinq'].isna().astype(int)
+        return df
+    ## 4 pub_rec_bankruptcies
+    def pub_rec_bankruptcies_fe(self, df):
+        df['pub_rec_bankruptcies'] = df['pub_rec_bankruptcies'].apply(lambda x: 0 if pd.isna(x) or x == 0.0 else 1)
+        return df
+    # --------------------------------
+    # categorical feature engineering 
+    ## 1. Term 
+    def term_fe(self, df):
+        # convert 36 months to 0 and 60 months to 1
+        df['term'] = df['term'].apply(lambda x: 0 if x == '36 months' else 1)
+        return df
+    
+    ## 2. grade 
+    def grade_feature_engineering(self, df):
+        # convert grade to numerical grade: 7 grades - A to G -> Convert to A:0, B:1, C:2, D:3, E:4, F:5, G:6
+        grade_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6}
+        df['grade'] = df['grade'].map(grade_dict)
+        return df
+    
+    ## 3. Home ownership
+    def home_ownership_fe(self, df):
+        # convert home ownership to numerical: 6 types - MORTGAGE, RENT, OWN, OTHER, NONE, ANY -> Convert to MORTGAGE:0, RENT:1, OWN:2, OTHER:3, NONE:4
+        home_ownership_dict = {'MORTGAGE': 0, 'RENT': 1, 'OWN': 2, 'OTHER': 3, 'NONE': 4}
+        df['home_ownership'] = df['home_ownership'].map(home_ownership_dict)
+        return df
+    
+    ## 4. verification_status
+    def verification_status_fe(self, df):
+        # convert verification_status to numerical: 3 types - Not Verified, Verified, Source Verified -> Convert to Not Verified:0, Verified:1, Source Verified:2
+        verification_status_dict = {'Not Verified': 0, 'Verified': 1, 'Source Verified': 2}
+        df['verification_status'] = df['verification_status'].map(verification_status_dict)
+        return df
+    
+    ## 6. earliest_cr_line
+    def earliest_cr_line_fe(self, df):
+    # convert to number of months from earliest credit line to the date of loan application
+        df['earliest_cr_line'] = pd.to_datetime(df['earliest_cr_line'])
+        df['issue_d'] = pd.to_datetime(df['issue_d'])  # Ensure 'issue_d' is also in datetime format
+        df['earliest_cr_line'] = (df['issue_d'] - df['earliest_cr_line']).dt.days / 30
+        return df
+        
+    ## 7. desc 
+    def desc_fe(self, df):
+        # convert desc to numerical: 2 types - NaN, not NaN -> Convert to NaN:0, not NaN:1
+        df['desc'] = df['desc'].isna().astype(int)
+        return df
+    
+    ## 8. emp_title
+    def emp_title_fe(self, df):
+        # convert emp_title to numerical: 2 types - NaN, not NaN -> Convert to NaN:0, not NaN:1 (job less vs having a job)
+        df['emp_title'] = df['emp_title'].isna().astype(int)
+        return df
+    
+    ## 9.mths_since_last_delinq
+    def mths_since_last_delinq_fe(self, df):
+        # convert mths_since_last_delinq to numerical: 2 types - NaN, not NaN -> Convert to NaN:0, not NaN:1
+        df['mths_since_last_delinq'] = df['mths_since_last_delinq'].isna().astype(int)
+        return df
+    
+    ## 10. last_credit_pull_d
+    def last_credit_pull_d_fe(self, df):
+        # if it is this month (Sep 2016), convert to 1, else convert to 0 (long ago)
+        df['last_credit_pull_d'] = df['last_credit_pull_d'].apply(lambda x: 1 if x == 'Sep-2016' else 0)
+        return df
+    
+    ## 11. next_pymnt_d
+    def next_pymnt_d_fe(self, df):
+        # if it is next month (Oct 2016), convert to 0, else convert to 1
+        df['next_pymnt_d'] = df['next_pymnt_d'].apply(lambda x: 1 if x == 'Oct-2016' else 0)
+        return df
+    
+    ## 12. issue_d
+    def months_between_issue_d_and_current_date(self, df):
+        df['issue_d'] = pd.to_datetime(df['issue_d'])
+        df['issue_d'] = (pd.to_datetime('2016-09-01') - df['issue_d']).dt.days / 30
+        return df
+    # 13. converting the target variable to numerical
+    def convert_target_to_numerical(self, y):
+        y = y.apply(lambda x: 1 if x == 'default' else 0)
+        return y
+    
+    # --------------------------------
+    
+    # Feature engineering pipeline 
+    def feature_engineering_pipeline(self, df):
+        df = self.drop_useless_numerical_cols(df)
+        df = self.convert_interest_rate_to_numerical(df)
+        df = self.emp_length_fe(df)
+        df = self.mths_since_last_delinq_fe(df)
+        df = self.pub_rec_bankruptcies_fe(df)
+        df = self.term_fe(df)
+        df = self.grade_feature_engineering(df)
+        df = self.home_ownership_fe(df)
+        df = self.verification_status_fe(df)
+        df = self.earliest_cr_line_fe(df)
+        df = self.desc_fe(df)
+        df = self.emp_title_fe(df)
+        df = self.mths_since_last_delinq_fe(df)
+        df = self.last_credit_pull_d_fe(df)
+        df = self.next_pymnt_d_fe(df)
+        df = self.months_between_issue_d_and_current_date(df)
+        return df
+        
+
+# Create a function to automate the pipeline (preq: preprocessor)
+def train_model(algorithm, preprocessor, X_train, y_train ):
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                                 ('classifier', algorithm)])
+    model = pipeline.fit(X_train, y_train)
+    return model
+
+# create a function to get prediciton 
+def get_predictions(model, X_test):
+    y_pred = model.predict(X_test)
+    y_pred_prob = model.predict_proba(X_test)[:, 1]
+    return y_pred, y_pred_prob
+
+# create a class to store eval metrics 
+class PerformanceMetrics:
+    def __init__(self, y_true, y_pred, y_pred_prob):
+        self.y_true = y_true
+        self.y_pred = y_pred
+        self.y_pred_prob = y_pred_prob
+        self.accuracy = accuracy_score(y_true, y_pred)
+        self.precision = precision_score(y_true, y_pred)
+        self.recall = recall_score(y_true, y_pred)
+        self.roc_auc = roc_auc_score(y_true, y_pred_prob)
+        self.f_1 = 2 * (self.precision * self.recall) / (self.precision + self.recall)
+    
+    def print_performance_metrics(self, model_name = None):
+        print(f'Performance Metrics of the model {model_name}: \n')
+        print(f'Accuracy: {self.accuracy:.4f}')
+        print(f'Precision: {self.precision:.4f}')
+        print(f'Recall: {self.recall:.4f}')
+        print(f'ROC AUC: {self.roc_auc:.4f}')
+        print(f'F1 Score: {self.f_1:.4f}')
